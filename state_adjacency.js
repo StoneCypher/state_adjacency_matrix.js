a873be968832562386ee8cd422694277132fae4f
John Haugeland <stonecypher@gmail.com>
2017-04-16 22:02:54 -0700

    add state labels

--- state_adjacency.js
+++ state_adjacency.js
@@ -1,4 +1,6 @@
 
+const states = ["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"];
+
 const state_adjacency = [
 
   [],
@@ -55,4 +57,4 @@ const state_adjacency = [
 
 ];
 
-export { state_adjacency };
+export { states, state_adjacency };
